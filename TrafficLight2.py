d4275216ddc4cab54ca1a1b0dee1be053e18a378
kxmdxhxx <kxmdxhxx@gmail.com>
2019-04-11 10:27:18 +0200

    Traffic Light (loop) script

---
+++ TrafficLight2.py
@@ -0,0 +1,32 @@
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+
+
+while True:
+	GPIO.setup(3, GPIO.OUT)  # Blue LED
+	GPIO.output(3, GPIO.HIGH)
+
+	time.sleep(2)
+
+	GPIO.output(3, GPIO.LOW)
+
+	GPIO.setup(5, GPIO.OUT)  # Yellow LED
+	GPIO.output(5, GPIO.HIGH)
+
+	time.sleep(2)
+
+	GPIO.output(5, GPIO.LOW)
+
+	GPIO.setup(7, GPIO.OUT)  # White LED
+	GPIO.output(7, GPIO.HIGH)
+
+	time.sleep(2)
+
+	GPIO.output(7, GPIO.LOW)
+
+	time.sleep(1)
+
+GPIO.cleanup()
