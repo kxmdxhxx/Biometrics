c1b9aa668c1dc369dd83bc65c061b04ce1f6e28b
kxmdxhxx <kxmdxhxx@gmail.com>
2019-04-11 10:40:50 +0200

    Traffic Light(infinite loop) script - added comments

--- TrafficLight2.py
+++ TrafficLight2.py
@@ -5,28 +5,28 @@ GPIO.setmode(GPIO.BOARD)
 
 
 
-while True:
+while True: # Infinite Loop for blink
 	GPIO.setup(3, GPIO.OUT)  # Blue LED
-	GPIO.output(3, GPIO.HIGH)
+	GPIO.output(3, GPIO.HIGH) # Turn on Blue LED
 
-	time.sleep(2)
+	time.sleep(2) # Sleep for 2 second
 
-	GPIO.output(3, GPIO.LOW)
+	GPIO.output(3, GPIO.LOW) # Turn off Blue LED
 
 	GPIO.setup(5, GPIO.OUT)  # Yellow LED
-	GPIO.output(5, GPIO.HIGH)
+	GPIO.output(5, GPIO.HIGH) # Turn on Yellow LED
 
-	time.sleep(2)
+	time.sleep(2) # Sleep for 2 second
 
-	GPIO.output(5, GPIO.LOW)
+	GPIO.output(5, GPIO.LOW) # Turn off Yellow LED
 
 	GPIO.setup(7, GPIO.OUT)  # White LED
-	GPIO.output(7, GPIO.HIGH)
+	GPIO.output(7, GPIO.HIGH) # Turn on White LED
 
-	time.sleep(2)
+	time.sleep(2) # Sleep for 2 second
 
-	GPIO.output(7, GPIO.LOW)
+	GPIO.output(7, GPIO.LOW) # Turn off White LED
 
-	time.sleep(1)
+	time.sleep(1) # Sleep for 1 second
 
 GPIO.cleanup()
